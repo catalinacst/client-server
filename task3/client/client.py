0001a3a71403abdaaf19a074aa7be1ea83f168aa
Catalina Castro <catalina.cst@utp.edu.co>
2020-02-21 00:37:24 -0500

    task3 - send binary file - list files server from client

--- task3/client/client.py
+++ task3/client/client.py
@@ -1,4 +1,5 @@
 import zmq
+import hashlib
 
 context = zmq.Context()
 
@@ -7,14 +8,60 @@ print("Conectando con el servidor...")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
+sizechunk = 2 * 1024 * 1024
+
 # read file
-file = open("file1m", "rb")
+namefile = "file1m"
+file = open(namefile, "rb")
+part = 0
 
-print("Enviando archivo...")
+action = input()
+
+if action == 'upload':
+	print("Enviando archivo...")
+elif action == 'list':
+	print("Listando archivos...")
+elif action == 'download':
+	print("Descargando archivo...")
+
 while True:
-	for line in file:
-		socket.send(line)
-		result = socket.recv()
-	break
+	if action == 'upload':
+		socket.send(b"%s" % action.encode('utf-8'))
+		ok = socket.recv()
+		chunk = file.read(sizechunk)
+		part = part + 1
+		if not chunk:
+			break
+		hashing = hashlib.md5(chunk).digest()
+		filename = namefile
+		filename = filename + ".part_" + str(part)
+		
+		# send filename, content and hash
+		socket.send_multipart([filename.encode(), chunk, hashing])
 
-print("Archivo enviado")
+		result = socket.recv()
+		print("Resultado: %s" % result)
+		print("Archivo enviado")
+	elif action == 'list':
+		# send action
+		socket.send(b"%s" % action.encode('utf-8'))
+		ok = socket.recv()
+
+		# send request
+		socket.send(b"lista archivos")
+		lenlistfiles = socket.recv_string()
+		print(lenlistfiles)
+		break
+	elif action == 'download':
+		socket.send(b"%s" % action.encode('utf-8'))
+		ok = socket.recv()
+
+		down_namefile = input()
+		socket.send_string("%s" % down_namefile)
+		ok = socket.recv()
+		print(ok)
+		break
+if action == 'upload':
+	file.close()
+
+
--- task3/server/server.py
+++ task3/server/server.py
@@ -1,19 +1,42 @@
 import time
 import zmq
-import math
+import hashlib
+from os import listdir
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
 while True:
-	#  Wait for next request from client
-	mess = socket.recv()
+	# recibir la accion
+	action = socket.recv()
+	action = action.decode('utf-8')
+	socket.send(b"Accion Recibida")
 
-	#  Do some 'work'
-	time.sleep(1)
+	# validar las acciones
+	if action == 'upload':
+		#  Wait for next request from client
+		filename, content, hashingclient = socket.recv_multipart()
+		print("Recibiendo Archivo...")
+		hashingserver = hashlib.md5(content).digest()
 
-	file = open("file",'ab')
-	file.write(mess)
-
-	socket.send(b"mensaje recibido")
+		if hashingclient == hashingserver:
+			file = open("files/" + filename.decode(), 'wb')
+			file.write(content)
+			file.close()
+			socket.send(b"Archivo Recibido")
+			print("Archivo Correcto")
+		else:
+			socket.send(b"Archivo Corrupto")
+			print("Archivo Corrupto")
+	elif action == 'list':
+		print("Enviando Lista Archivos...")
+		ok = socket.recv()
+		listfiles = listdir("files/")
+		socket.send_string("%s" % str(listfiles))
+	elif action == 'download':
+		print("Solicitud Descarga Archivo...")
+		down_namefile = socket.recv_string()
+		# conocer cantidad de partes que tiene el archivo a descargar
+		# file = open("files/" + down_namefile, "rb")
+		socket.send(b"ok")
